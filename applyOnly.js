98bf85dc08b71d774a4b0d5d14b5153ad5250970
Ed Spencer <edward@midlandswebdesign.com>
2009-04-23 11:09:02 +0100

    Defined Ext.applyOnly and added a few unit tests

---
+++ applyOnly.js
@@ -0,0 +1,16 @@
+/**
+ * Applies only a pre-specified set of properties from one object to another
+ * @param {Object} receiver The object to copy the properties to
+ * @param {Object} sender The object to copy the properties from
+ * @param {Array} whitelist The whitelist of properties to copy (e.g. ['width', 'height'])
+ * @return {Object} The receiver object, with any of the whitelisted properties overwritten if they exist in sender
+ */
+Ext.applyOnly = function(receiver, sender, whitelist) {
+  if (receiver && sender) {
+    Ext.each(whitelist || [], function(item) {
+      if (sender[item]) receiver[item] = sender[item];
+    }, this);
+  };
+  
+  return receiver;
+};
---
+++ applyOnly.spec.js
@@ -0,0 +1,44 @@
+Screw.Unit(function() {
+  describe("Ext.applyOnly", function() {
+    var sender = {
+      height:   200,
+      width:    200,
+      message:  "A new test",
+      closable: true
+    };
+    
+    var whitelist = ['height', 'width'];
+    
+    it("should update whitelisted properties", function() {
+      var res = Ext.applyOnly(receiver(), sender, whitelist);
+      
+      expect(res.height).to(equal, 200);
+      expect(res.width).to(equal, 200);
+    });
+    
+    it("should not update any property which is not in the whitelist", function() {
+      var res = Ext.applyOnly(receiver(), sender, whitelist);
+      
+      expect(res.message).to(equal, "This is a test");
+      expect(res.closable).to(equal, false);
+    });
+    
+    it("should not overwrite receiver properties if they are not present in the sender", function() {
+      var res = Ext.applyOnly(receiver(), {height: 200}, whitelist);
+      
+      expect(res.width).to(equal, 100);
+    });
+    
+    /**
+     * Creates a new receiver object for each test as applyOnly would otherwise overwrite it each time
+     */
+    function receiver() {
+      return {
+        height:   100,
+        width:    100,
+        message:  "This is a test",
+        closable: false
+      };
+    }
+  });
+});
